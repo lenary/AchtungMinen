a11156aebcf87b62f79ce10de058513dd6fab779
Sam Elliott <sam@lenary.co.uk>
2013-05-05 20:36:31 +0100

    Lots of progress. Only one function left (roughly)

--- AchtungMinen/Game.hs
+++ AchtungMinen/Game.hs
@@ -1,121 +1,139 @@
-module AchtungMinen.Game
-( play
-, score
-) where
+{-# LANGUAGE GADTs #-}
+
+module AchtungMinen.Game where
 
 import AchtungMinen.World
 
 import Control.Applicative
+import Control.Monad.State
 import qualified Control.Monad.Random as R
 import qualified Data.Map as M
 import qualified Data.List as L
 
-data GameState = InProgress { field :: Field
-                            , fog :: Mask
-                            }
-               | Finished { won_game :: Bool
-                          , mines_found :: Int
-                          , coords_cleared :: Int
-                          }
-               deriving (Show)
-
+-- * Some Types
 type Map a = M.Map Coord a
 
+-- | A Square either Contains a Mine, or has N mines surrounding it
 data Square = Mine
             | Clue Int
             deriving (Show,Eq)
 
+-- | A Field is a Map of Coordinates to Mines or Clues
 type Field = Map Square
-type Mask  = Map Bool
 
--- Public API
+-- | A Mask is a Map of Coordinates to Booleans
+type Fog  = Map Bool
 
-play :: Player p => p -> IO (Bool, Int, Int)
-play _ = return (False, 0, 0)
+type Result = (Bool, Int, Int)
 
-score :: (Bool, Int, Int) -> Int
-score (won, found, cleared) = cleared + 10 * found + (if won then 100 else 0)
+-- * Public API
 
--- Field Generation
+-- | Plays a player against a field
+play :: Player p => p -> IO Result
+play p = do
+  st <- newTrial $ newContestant p
+  ft <- snd <$> runStateT playTrial st
+  return $ scoreTrial ft
 
+-- | Gives a score to the result from `play`
+score :: Result -> Int
+score (won, correct, incorrect) = (2 * correct - incorrect) * (if won then 3 else 1)
+
+-- * Minefields
+-- ** Generation
+
+-- | Creates a (mine)field empty of mines
 emptyField :: Field
 emptyField = M.fromList [ (c,Clue 0) | c <- allCoords ]
 
+-- | Creates a (mine)field with randomly placed mines and correctly calculated clues
 genField :: IO Field
 genField = genClues <$> genMines emptyField
 
+-- | Adds `mineCount` mines to a field, randomly placing them
 genMines :: Field -> IO Field
-genMines b = iterate addMine (pure b) !! mineCount
+genMines f = iterate addMine (pure f) !! mineCount
 
+-- | Updates clues to a field already containing mines
 genClues :: Field -> Field
-genClues b = M.mapWithKey (addClue b) b
+genClues f = M.mapWithKey (addClue f) f
 
+-- | Adds a single mine to a field
 addMine :: IO Field -> IO Field
-addMine ib = do
-  b <- ib
-  c <- randomFromList $ safeSquares b
-  return $ M.insert c Mine b
+addMine iof = do
+  f <- iof
+  c <- randomFromList $ safeSquares f
+  return $ M.insert c Mine f
 
+-- | Adds a single clue to a field at Coord
 addClue :: Field -> Coord -> Square -> Square
 addClue _ _ Mine = Mine
-addClue b c _    = Clue $ countMinedNeighbours b c
+addClue f c _    = Clue $ countMinedNeighbours f c
 
+-- | Count neighbour squares that contain mines
+countMinedNeighbours :: Field -> Coord -> Int
+countMinedNeighbours f c =
+  length
+  $ minedSquares f `L.intersect` neighbours c
+
+safeSquares, minedSquares :: Field -> [Coord]
+safeSquares  = M.keys . M.filter (/= Mine)
+minedSquares = M.keys . M.filter (== Mine)
+
+-- | Get a random element from a list
 randomFromList :: [a] -> IO a
 randomFromList lst = R.fromList $ map (\e -> (e, 1)) lst
 
-countMinedNeighbours :: Field -> Coord -> Int
-countMinedNeighbours b c =
-  length
-  $ minedSquares b `L.intersect` neighbours c
+-- ** Revealing Logic
 
-safeSquares, minedSquares :: Field -> [Coord]
-safeSquares = M.keys . M.filter (/= Mine)
-minedSquares = M.keys . M.filter (== Mine)
+revealSquares :: Field -> Fog -> Coord -> ([(Coord, Int)], Fog)
+revealSquares = undefined -- TODO
 
--- Revealing Logic
+reveal :: Field -> Coord -> Fog -> Fog
+reveal f c m = keepHoles m newFog
+  where
+    newFog = makeFog $ revealOne f c
 
-reveal :: Field -> Coord -> Mask
-reveal b start = reveal' b [start] fullMask
+revealOne :: Field -> Coord -> [Coord]
+revealOne f c = revealOne' f [c] []
 
-reveal' :: Field -> [Coord] -> Mask -> Mask
-reveal' _ []            found = found
-reveal' b (search:next) found | isEmpty b search = reveal' b (more ++ next) (M.insert search True found)
-                              | otherwise        = reveal' b next           (M.insert search True found)
+revealOne' :: Field -> [Coord] -> [Coord] -> [Coord]
+revealOne' f []            found = found
+revealOne' f (search:rest) found | isEmpty f search = revealOne' f (rest ++ more) (search:found)
+                                 | otherwise        = revealOne' f rest           (search:found)
   where
     more = [c | c <- neighbours search
-              , c `notElem` M.keys (M.filter id found)
-              , c `notElem` next
+              , c `notElem` found
+              , c `notElem` rest
               , c `elem` allCoords
               ]
 
-isEmpty :: Field -> Coord -> Bool
-isEmpty b c = M.lookup c b == (Just (Clue 0))
+-- ** Masks
 
--- Masks
+makeFog :: [Coord] -> Fog
+makeFog cs = M.fromList [(c,c`elem`cs) | c <- allCoords]
 
-makeMask :: [Coord] -> Mask
-makeMask cs = M.fromList [(c,c`elem`cs) | c <- allCoords]
+holeyFog, fullFog :: Fog
+holeyFog = M.fromList [(c,True)  | c <- allCoords]
+fullFog  = M.fromList [(c,False) | c <- allCoords]
 
-holeyMask, fullMask :: Mask
-holeyMask = M.fromList [(c,True)  | c <- allCoords]
-fullMask  = M.fromList [(c,False) | c <- allCoords]
+keepHoles, keepFilled :: Fog -> Fog -> Fog
+-- | Where there are holes, keep them
+keepHoles = M.unionWith (||)
+-- | Where there are filled squares, keep them
+keepFilled = M.unionWith (&&)
 
-subtractMasks :: Mask -> Mask -> Mask
-subtractMasks = M.unionWith (||)
+holes, filled :: Fog -> [Coord]
+holes  = M.keys . M.filter (id)
+filled = M.keys . M.filter (not)
 
--- Printing
+-- ** Printing
 
-printField :: Field -> IO ()
-printField = flip printBoth $ holeyMask
-
-printOverlay :: Mask -> IO ()
-printOverlay = printBoth emptyField
-
-printBoth :: Field -> Mask -> IO ()
-printBoth b m =
+printField :: Field -> Fog -> IO ()
+printField f m =
   putStr
   $ delinate (x maxCoord + 2)
-  [ shSq (M.lookup (x',y') m) (M.lookup (x',y') b)
+  [ shSq (M.lookup (x',y') m) (M.lookup (x',y') f)
     | y' <- [0..(y maxCoord + 1)]
     , x' <- [0..(x maxCoord + 1)]
     ]
@@ -129,3 +147,138 @@ printBoth b m =
     delinate  _  [] = []
     delinate len xs = let (before,after) = splitAt len xs
                       in before ++ "\n" ++ delinate len after
+
+-- * Move-based functions
+
+isMine, isClue, isEmpty :: Field -> Coord -> Bool
+isMine  f c = M.lookup c f == (Just Mine)
+isClue  f c = not $ isMine f c
+isEmpty f c = M.lookup c f == (Just (Clue 0))
+
+isInBoard :: Coord -> Bool
+isInBoard c = c `elem` allCoords
+
+-- * Contestant Functions
+
+data Contestant where
+  C :: Player p => p -> Contestant
+
+instance Player Contestant where
+  sendMove (C p) = sendMove p >>= \(p', m) -> return (C p', m)
+  recvResp r (C p) = C <$> recvResp r p
+
+newContestant :: Player p => p -> Contestant
+newContestant p = C p
+
+-- * Trial Functions
+
+data Trial = Trial
+    { player :: Contestant
+    , field  :: Field
+    , fog    :: Fog
+    , flags  :: [Coord]
+    , dead   :: Bool -- ?
+    , movesLeft :: Int -- ?
+    }
+
+type TrialM = StateT Trial IO
+
+-- | Starts a new trial with a given contestant
+newTrial :: Contestant -> IO Trial
+newTrial c = do
+  f <- genField
+  return Trial
+    { player = c
+    , field  = f
+    , fog    = fullFog
+    , flags  = []
+    , dead   = False
+    , movesLeft = (x maxCoord) * (y maxCoord)
+    }
+
+-- | Play a game until it finishes
+playTrial :: TrialM ()
+playTrial = do
+  moves_left <- gets movesLeft
+  is_dead <- gets dead
+  all_done <- flaggedAll
+  if is_dead || all_done || moves_left <= 0
+    then return ()
+    else playTurn >> playTrial
+
+-- | Get the player's next move, see what happens
+playTurn :: TrialM ()
+playTurn = do
+  move <- getNextMove
+  resp <- case move of
+            (Flag c) -> flagCoord c
+            (Clear c) -> clearCoord c
+  deliverResponse resp
+
+
+
+-- | Get the player's next move
+getNextMove :: TrialM Move
+getNextMove = do
+  t <- get
+  (p, m) <- liftIO $ sendMove $ player t
+  put t { player = p }
+  return m
+
+-- | Send the response to a move to a player
+deliverResponse :: Response -> TrialM ()
+deliverResponse resp = do
+  t <- get
+  p' <- liftIO $ recvResp resp $ player t
+  put t { player = p'
+        , movesLeft = (movesLeft t) - 1
+        }
+
+-- | Flag a given coordinate
+flagCoord :: Coord -> TrialM Response
+flagCoord c = do
+  liftIO $ print ("Flagged", c)
+  t <- get
+  put t { flags = L.nub (c:flags t) }
+  return Flagged
+
+-- | Have they flagged all the mines (extra flags are ok)
+flaggedAll :: TrialM Bool
+flaggedAll = do
+  correct <- gets correctFlaggings
+  return (correct == mineCount)
+
+-- | The player is attempting to clear `c` - they'll die or they'll reveal more squares
+clearCoord :: Coord -> TrialM Response
+clearCoord c = do
+  field <- gets field
+  if c `elem` minedSquares field
+    then playerDied
+    else revealField c
+
+-- | The player died. Get the mines to return to the player
+playerDied :: TrialM Response
+playerDied = do
+  liftIO $ print "Died"
+  t <- get
+  put t { dead = True }
+  let mines = minedSquares (field t)
+  return (Died mines)
+
+-- | The player has not died: reveal more squares
+revealField :: Coord -> TrialM Response
+revealField c = do
+  liftIO $ print ("Revealed", c)
+  t <- get
+  let (cleared, fog') = revealSquares (field t) (fog t) c
+  put t { fog = fog' }
+  return (Cleared cleared)
+
+-- | Convert the result of a trial into something `score` will understand
+scoreTrial :: Trial -> Result
+scoreTrial t = (dead t, correctFlaggings t, incorrectFlaggings t)
+
+-- | Counts the number of squares the player flagged successfully
+correctFlaggings, incorrectFlaggings :: Trial -> Int
+correctFlaggings t = length $ flags t `L.intersect` minedSquares (field t)
+incorrectFlaggings t = length $ flags t L.\\ minedSquares (field t)
--- AchtungMinen/World.hs
+++ AchtungMinen/World.hs
@@ -1,36 +1,49 @@
+-- | Utility functions? Never!
 module AchtungMinen.World where
 
+-- | A coordinate in 2d space
 type Coord = (Int, Int)
 
-data Play = Move Coord
-          | Mark Coord
+-- | What is the player's next move?
+data Move = Clear Coord
+          | Flag Coord
           deriving (Show)
 
-data Response = Marked
-              | Safe [(Coord, Int)]
-              | Dead
+-- | How did your last move go?
+--
+-- Did you flag something? Did you clear some spaces?
+data Response = Flagged
+              | Cleared [(Coord, Int)]
+              | Died [Coord]
               deriving (Show)
 
+-- | All players should implement this typeclass in order to play
 class Player p where
-  -- |'sendMove' asks the player for their next move
-  sendMove :: p -> IO (p, Play)
-  -- |'recvResp' lets the player know what happened
+  -- | 'sendMove' asks the player for their next move
+  sendMove :: p -> IO (p, Move)
+  -- | 'recvResp' lets the player know what happened
   recvResp :: Response -> p -> IO p
 
+-- | The board is this size (`fst` * `snd`) in squares
 maxCoord :: Coord
 maxCoord = (9, 9)
 
+-- | There are this many mines to be found each game
 mineCount :: Int
 mineCount = 10
 
 x, y :: Coord -> Int
+-- | Retrieve the x component of a coordinate
 x = fst
+-- | Retrieve the y component of a coordinate
 y = snd
 
+-- | What are all the coordinates in the game?
 allCoords :: [Coord]
 allCoords = [(x', y') | x' <- [1..(x maxCoord)],
                         y' <- [1..(y maxCoord)]]
 
+-- | Which are the coordinates of my current square (doesn't respect boundaries)
 neighbours :: Coord -> [Coord]
 neighbours c = [ (x',y') | x' <- [(x c - 1)..(x c + 1)]
                          , y' <- [(y c - 1)..(y c + 1)]
